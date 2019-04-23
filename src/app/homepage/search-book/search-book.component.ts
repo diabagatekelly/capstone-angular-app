7f0351ac65276220168a934049ecf64e842aa4cb
Kelly Diabagate <diabagatekelly@yahoo.fr>
2019-04-23 10:26:07 -0500

    Fix stackblitz relative links

--- src/app/homepage/search-book/search-book.component.ts
+++ src/app/homepage/search-book/search-book.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { GoodreadsBooksService } from 'src/app/shared/goodreads-books.service';
+import { Component, OnInit } from '@angular/core';
+import { GoodreadsBooksService } from '../../shared/goodreads-books.service';
 
 @Component({
   selector: 'app-search-book',
